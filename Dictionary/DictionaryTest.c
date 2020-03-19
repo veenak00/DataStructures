2cbf0a11dd2c90c70094ed4e22359c8ff4ed2fbd
veenak00 <52979031+veenak00@users.noreply.github.com>
2020-03-19 11:27:41 -0700

    Add files via upload

---
+++ Dictionary/DictionaryTest.c
@@ -0,0 +1,78 @@
+/*Name: Veena Kotha
+Cruz ID: vkotha
+Class: 12M 
+Date: 6/1/19
+Description:  The name of this file is DictionaryTest.c.
+   -The role of this file for this project is to test Dictionary.c.
+*/ 
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"Dictionary.h"
+
+#define MAX_LEN 180
+
+int main(int argc, char* argv[]){
+ 
+   /*Dictionary A = newDictionary();
+   char* k;
+   char* v;
+   char* word1[] = {"one","two","three","four","five","six","seven"};
+   char* word2[] = {"a","b","c","d","e","f","g"};
+   int i;
+   char* ex[] = {"hi", "bye"};
+   char* ex2[] = {"one", "two"};
+   int index;
+   
+   for(index=0; index<2; i++){
+      insert(A, ex[index], ex2[index]);
+   }
+
+   printDictionary(stdout, A);
+
+
+
+   printf("%s\n", (isEmpty(A)?"true":"false"));
+
+   for(i=0; i<7; i++){
+      insert(A, word1[i], word2[i]);
+   }
+
+   
+
+   printDictionary(stdout, A);
+
+   for(i=0; i<7; i++){
+      k = word1[i];
+      v = lookup(A, k);
+      printf("key=\"%s\" %s\"%s\"\n", k, (v==NULL?"not found ":"value="), v);
+   }
+
+   // insert(A, "five", "glow"); // error: key collision
+
+   delete(A, "one");
+   delete(A, "two")
+   delete(A, "four");
+   delete(A, "seven");
+
+   //printDictionary(stdout, A);
+
+   for(i=0; i<7; i++){
+      k = word1[i];
+      v = lookup(A, k);
+      printf("key=\"%s\" %s\"%s\"\n", k, (v==NULL?"not found ":"value="), v);
+   }
+
+   // delete(A, "one");  // error: key not found
+
+   printf("%s\n", (isEmpty(A)?"true":"false"));
+   printf("%d\n", size(A));
+   makeEmpty(A);
+   printf("%s\n", (isEmpty(A)?"true":"false"));
+
+   freeDictionary(&A);
+
+   return(EXIT_SUCCESS);
+*/
+}
